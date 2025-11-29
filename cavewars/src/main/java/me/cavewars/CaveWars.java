18c4fad4bd15072a732ad18ffac7fbd77b05c45a
podpaska <kaerolastaff@gmail.com>
2025-11-30 00:57:46 +0100

    Delete cavewars directory

--- cavewars/src/main/java/me/cavewars/CaveWars.java
+++
@@ -1,6 +0,0 @@
-package me.cavewars;
-import org.bukkit.plugin.java.JavaPlugin;
-public class CaveWars extends JavaPlugin {
-    @Override public void onEnable() { getLogger().info("CaveWars enabled"); }
-    @Override public void onDisable() { }
-}
